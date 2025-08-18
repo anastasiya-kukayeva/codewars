59547be4ebdc5ef18e54c3f206c2f9e63a03b2f9
Anastasiya_Kukayeva <anastasiya.kukayeva@gmail.com>
2025-08-18 17:49:56 +0300

    Added first Even or Odd function

--- codewars.js
+++ codewars.js
@@ -3,3 +3,14 @@
 function multiply(a, b){
     return a * b
 }
+
+
+// Even or Odd
+
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
+        return "Even"
+    } else {
+        return "Odd"
+    }
+}
